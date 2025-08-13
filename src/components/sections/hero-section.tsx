207b07310a4d384f430a073d9e1b96381ff6ea41
Cory Pride <cory.pride83@gmail.com>
2025-08-13 00:10:55 +0000

    when on larger screens the image size should be the equivialet of the wh

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
     >
       <div className="absolute inset-0 bg-black/60" />
       <div className="container relative z-10 px-4 md:px-6">
-        <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:gap-24">
+        <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:gap-24 lg:items-start">
           <div className="flex flex-col justify-center space-y-4 text-center lg:text-left">
             <div className="space-y-2">
               <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none font-headline text-yellow-500">
@@ -32,16 +32,14 @@ export default function HeroSection() {
               </div>
             </div>
           </div>
-          <div className="flex items-center justify-center">
-            <div className="relative w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-96 lg:h-96">
-                <Image
-                src="/img/logo_dark.png"
-                fill
-                alt="Picture Me Perfect"
-                className="mx-auto aspect-square overflow-hidden rounded-xl object-cover"
-                data-ai-hint="photo booth"
-                />
-            </div>
+          <div className="relative w-full h-full min-h-[250px] md:min-h-[400px]">
+            <Image
+              src="/img/logo_dark.png"
+              fill
+              alt="Picture Me Perfect"
+              className="mx-auto aspect-square overflow-hidden rounded-xl object-cover"
+              data-ai-hint="photo booth"
+            />
           </div>
         </div>
       </div>
